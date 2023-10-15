fc22be1483316fc64382e4a08d216467c1c4d06a
Gowda_Chaya_Kumar_A <47766833+ck989@users.noreply.github.com>
2023-10-14 20:24:37 -0700

    Create rotaterightleftbits.c

---
+++ C programming/rotaterightleftbits.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#define INT_BITS 8
+
+uint8_t leftrotate(uint8_t x, uint8_t d)
+{
+    return ((x << d) | (x >> (INT_BITS - d)));
+}
+
+
+uint8_t rightrotate(uint8_t x, uint8_t d)
+{
+    return ((x >> d) | (x << (INT_BITS - d)));
+}
+
+int main()
+{
+    printf("res1 : 0x%x\n\r", leftrotate(7,2));
+    printf("res2 : 0x%x", rightrotate(7,2));
+    
+    return 0;
+}
