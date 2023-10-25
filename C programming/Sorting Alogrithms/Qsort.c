68a239f1d5a80e01672aaed6ae8e26b06e72e104
Gowda_Chaya_Kumar_A <47766833+ck989@users.noreply.github.com>
2023-10-25 01:03:30 -0700

    Update Qsort.c

--- C programming/Sorting Alogrithms/Qsort.c
+++ C programming/Sorting Alogrithms/Qsort.c
@@ -11,32 +11,35 @@ void swap(int* x, int* y){
 }
 
 int partition(int* arr, int start, int end){
-    int pivot = arr[end];
-    int pIndex = 0;
+    int pIndex = start;
+    int Pivot = arr[end];
     
-    for(int i = 0; i < end; i++){
-        if(arr[i] <= pivot){
+    for(int i = start; i < end; i++){
+        if(arr[i] <= Pivot){
             swap(&arr[i], &arr[pIndex]);
             pIndex++;
         }
     }
     swap(&arr[pIndex], &arr[end]);
+    
     return pIndex;
 }
 
-void Qsort(int* arr, int start, int end){
+void QSort(int* arr, int start, int end){
     int pIndex = 0;
-    if (start < end){
+    
+    if(start < end){
         pIndex = partition(arr, start, end);
-        Qsort(arr, start, pIndex - 1);
-        Qsort(arr, pIndex + 1, end);
+        QSort(arr, start, pIndex - 1);
+        QSort(arr, pIndex + 1, end);
     }
+    
 }
 
 int main() {
     
     int arr[MAX_ARRAY_SZ] = {3, 6, 8, 2, 5, 1, 4, 7};
-    Qsort(arr, 0, MAX_ARRAY_SZ - 1);
+    QSort(arr, 0, MAX_ARRAY_SZ - 1);
     for(int i = 0; i < MAX_ARRAY_SZ; i++){
         printf("%d\t",arr[i]);
     }
