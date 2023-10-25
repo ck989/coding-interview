42018114b9c8b41d36f6982141d1bbcf127faa4d
Gowda_Chaya_Kumar_A <47766833+ck989@users.noreply.github.com>
2023-10-25 15:15:50 -0700

    Update Reverse words.c

--- C programming/Strings/Reverse words.c
+++ C programming/Strings/Reverse words.c
@@ -18,24 +18,29 @@ void Reversewords(char* str, int start, int end){
     
     int i = 0, j = 0;
     while(str[i] != '\0'){
-        while(i < end && str[i] == ' ')
-            i++;
+        while(i < end && str[i] == ' ') i++; //check if there is any prepending space and eliminate it
         
         j = i + 1;
-        while(j < end && str[j] != ' ')
-            j++;
+        while(j < end && str[j] != ' ') j++; //iterate until you find a space
         
-        strrev(&str[i], i, j - i - 1);
-        i = j + 1;
+        if(j == end) j++; //account the last missing char for reversing
+        
+        strrev(str, i, j - 1); //reverse only the selected range of indices
+        i = j + 1; //skip space
     }
 }
 
 int main() {
     
-    char str[20] = "This book is mine";
+    char str[50] = "This book is mine, the ruler of the world";
     int len = strlen(str);
     
     Reversewords(str, 0, len - 1);
     printf("%s",str);
     return 0;
 }
+
+/*
+output:
+world the of ruler the mine, is book This
+*/
